8bf2094b9fd59642ceb96cd9a273202b40e4d315
Pc夏俊炎 <xjy@kswyit.com>
2018-11-01 16:43:12 +0800

    暂无初始页面 直接跳转到admin 后台 增加后台登录界面

---
+++ src/WYFast.Web/Areas/Web/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WYCore.Web.Areas.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        /// <summary>
+        /// 初始页面
+        /// </summary>
+        /// <returns></returns>
+        public IActionResult Index()
+        {
+            return Redirect("/Admin/Login/Index");
+        }
+    }
+}
